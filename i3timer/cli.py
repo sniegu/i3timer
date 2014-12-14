a1f810b3dbe4f0b94bd1565d2bf3487b2d515a65
Piotr Śniegowski <psnieg@man.poznan.pl>
2014-12-14 21:41:38 +0100

    Minor changes.

--- i3timer/cli.py
+++ i3timer/cli.py
@@ -3,33 +3,36 @@
 import argparse
 from i3timer import utils
 
-parser = argparse.ArgumentParser('i3timer', description='a modern timer')
-parser.add_argument('-d', '--daemon', dest='daemon', default=False, action='store_true')
+def run():
+    parser = argparse.ArgumentParser('i3timer', description='a modern timer')
+    parser.add_argument('-d', '--daemon', dest='daemon', default=False, action='store_true')
 
-parser.add_argument('-s', '--start', dest='action', default=None, action='store_const', const='start')
-parser.add_argument('-t', '--stop', dest='action', default=None, action='store_const', const='stop')
-parser.add_argument('-p', '--pause', dest='action', default=None, action='store_const', const='pause')
-parser.add_argument('-g', '--toggle', dest='action', default=None, action='store_const', const='toggle')
+    parser.add_argument('-s', '--start', dest='action', default=None, action='store_const', const='start')
+    parser.add_argument('-t', '--stop', dest='action', default=None, action='store_const', const='stop')
+    parser.add_argument('-p', '--pause', dest='action', default=None, action='store_const', const='pause')
+    parser.add_argument('-g', '--toggle', dest='action', default=None, action='store_const', const='toggle')
 
-parser.add_argument('-e', '--shell', dest='shell', default=False, action='store_true')
-parser.add_argument('-c', '--current', dest='current', default=False, action='store_true')
+    parser.add_argument('-e', '--shell', dest='shell', default=False, action='store_true')
+    parser.add_argument('-c', '--current', dest='current', default=False, action='store_true')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
 
-if args.daemon:
-    utils.listen_forever()
+    if args.daemon:
+        utils.listen_forever()
+    else:
+        client = utils.TimerClient()
 
-if args.action is not None:
+        if args.shell:
+            from IPython import embed
+            embed()
 
-    client = utils.TimerClient()
+        if args.current:
+            print(client.state['current'])
 
-    if args.shell:
-        from IPython import embed ; embed()
+        if args.action is not None:
+            client.execute(args.action)
 
-    if args.current:
-        print(client.state['current'])
 
-    if args.action is not None:
-        client.execute(args.action)
-
+if __name__ == '__main__':
+    run()
